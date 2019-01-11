09bdd4c41f18681d9930b0041c6643b9f5a6e5f9
IrinaYukh <40866840+IrinaYukh@users.noreply.github.com>
2019-01-11 12:04:25 +0200

    Created DataProvider class with csv file with data for login test

---
+++ src/test/java/ru/stqa/selenium/DataProviders.java
@@ -0,0 +1,54 @@
+package ru.stqa.selenium;
+
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class DataProviders {
+    @DataProvider
+    public static Iterator<Object[]> positiveAuthorization() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/AuthorizationData.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+    @DataProvider
+    public static Iterator<Object[]> positiveRegistrationSeller() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                DataProviders.class.getResourceAsStream("/RegistrationSeller.data")));
+
+        List<Object[]> userData = new ArrayList<Object[]>();
+        String line = in.readLine();
+        while (line != null) {
+            userData.add(line.split(";"));
+            line = in.readLine();
+        }
+        in.close();
+        return userData.iterator();
+    }
+
+    
+    private Object generateRandomPassword() {
+        return "pass" + new Random().nextInt();
+    }
+    //---------------End--------------------//
+
+    private Object generateRandomName() {
+        return "demo" + new Random().nextInt();
+    }
+}
+
